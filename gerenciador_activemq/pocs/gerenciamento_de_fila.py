955a8668ed75ad41661b68b73fda777dc7d16f42
Rafael Coelho <rafaellcoellho@gmail.com>
2023-04-14 22:24:33 -0300

    adiciona tipos

--- gerenciador_activemq/pocs/gerenciamento_de_fila.py
+++ gerenciador_activemq/pocs/gerenciamento_de_fila.py
@@ -1,8 +1,8 @@
 import stomp
 
 
-def criar_fila(gerenciador_de_conexao):
-    nome_da_fila = input("Digite o nome da fila: ")
+def criar_fila(gerenciador_de_conexao: stomp.Connection):
+    nome_da_fila: str = input("Digite o nome da fila: ")
 
     gerenciador_de_conexao.send(
         body="",
@@ -12,8 +12,8 @@ def criar_fila(gerenciador_de_conexao):
     print(f"Fila {nome_da_fila} criada com sucesso")
 
 
-def criar_topico(gerenciador_de_conexao):
-    nome_do_topico = input("Digite o nome do tópico: ")
+def criar_topico(gerenciador_de_conexao: stomp.Connection):
+    nome_do_topico: str = input("Digite o nome do tópico: ")
 
     gerenciador_de_conexao.send(
         body="",
@@ -23,31 +23,31 @@ def criar_topico(gerenciador_de_conexao):
     print(f"Tópico {nome_do_topico} criado com sucesso")
 
 
-def mostrar_quantidade_de_mensagens(gerenciador_de_conexao):
+def mostrar_quantidade_de_mensagens(gerenciador_de_conexao: stomp.Connection):
     print("executando mostrar_quantidade_de_mensagens")
 
 
-def assinar_topico(gerenciador_de_conexao):
+def assinar_topico(gerenciador_de_conexao: stomp.Connection):
     print("executando assinar_topico")
 
 
-def enviar_mensagem_para_cliente(gerenciador_de_conexao):
+def enviar_mensagem_para_cliente(gerenciador_de_conexao: stomp.Connection):
     print("executando enviar_mensagem_para_cliente")
 
 
-def enviar_mensagem_para_topico(gerenciador_de_conexao):
+def enviar_mensagem_para_topico(gerenciador_de_conexao: stomp.Connection):
     print("executando enviar_mensagem_para_topico")
 
 
 def modo_exemplo_gerente_da_fila():
     print("executando gerente da fila")
-    gerenciador_de_conexao = stomp.Connection()
+    gerenciador_de_conexao: stomp.Connection = stomp.Connection()
 
     print("Iniciando conexão com ActiveMQ")
-    gerenciador_de_conexao.connect("admin", "admin", wait=True)
+    gerenciador_de_conexao.connect(username="admin", passcode="admin", wait=True)
     print("Conectado")
 
-    executando = True
+    executando: bool = True
     while executando:
         try:
             print("=====================================")
@@ -56,7 +56,7 @@ def modo_exemplo_gerente_da_fila():
             print("3 - Mostrar quantidade de mensagens nas filas/tópicos")
             print("0 - Sair")
             print("=====================================")
-            opcao = input("Escolha uma opção: ")
+            opcao: str = input("Escolha uma opção: ")
             if opcao == "1":
                 criar_fila(gerenciador_de_conexao)
             elif opcao == "2":
@@ -77,13 +77,13 @@ def modo_exemplo_gerente_da_fila():
 
 def modo_exemplo_cliente_da_fila():
     print("executando cliente da fila")
-    gerenciador_de_conexao = stomp.Connection()
+    gerenciador_de_conexao: stomp.Connection = stomp.Connection()
 
     print("Iniciando conexão com ActiveMQ")
-    gerenciador_de_conexao.connect("admin", "admin", wait=True)
+    gerenciador_de_conexao.connect(username="admin", passcode="admin", wait=True)
     print("Conectado")
 
-    executando = True
+    executando: bool = True
     while executando:
         try:
             print("=====================================")
@@ -92,7 +92,7 @@ def modo_exemplo_cliente_da_fila():
             print("3 - Enviar mensagem para tópico")
             print("0 - Sair")
             print("=====================================")
-            opcao = input("Escolha uma opção: ")
+            opcao: str = input("Escolha uma opção: ")
             if opcao == "1":
                 assinar_topico(gerenciador_de_conexao)
             elif opcao == "2":
