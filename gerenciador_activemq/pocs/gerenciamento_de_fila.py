40218c2d3f3d69e0d95588e6a7af36df88f830cd
Rafael Coelho <rafaellcoellho@gmail.com>
2023-04-16 15:36:11 -0300

    inicia novamente do zero gerente

--- gerenciador_activemq/pocs/gerenciamento_de_fila.py
+++ gerenciador_activemq/pocs/gerenciamento_de_fila.py
@@ -1,46 +1,23 @@
 import stomp
 
 
-def criar_fila(gerenciador_de_conexao: stomp.Connection):
-    nome_da_fila: str = input("Digite o nome da fila: ")
+def listar_filas_e_qtd_mensagens(gerenciador_de_conexao: stomp.Connection):
+    print("executando listar_filas_e_qtd_mensagens")
 
-    gerenciador_de_conexao.send(
-        body="",
-        destination=f"/queue/{nome_da_fila}",
-        headers={"persistent": "true"},
-    )
-    print(f"Fila {nome_da_fila} criada com sucesso")
+
+def listar_topicos_e_qtd_mensagens(gerenciador_de_conexao: stomp.Connection):
+    print("executando listar_topicos_e_qtd_mensagens")
+
+
+def criar_fila(gerenciador_de_conexao: stomp.Connection):
+    print("executando criar_fila")
 
 
 def criar_topico(gerenciador_de_conexao: stomp.Connection):
-    nome_do_topico: str = input("Digite o nome do tópico: ")
-
-    gerenciador_de_conexao.send(
-        body="",
-        destination=f"/topic/{nome_do_topico}",
-        headers={"persistent": "true"},
-    )
-    print(f"Tópico {nome_do_topico} criado com sucesso")
-
-
-def mostrar_quantidade_de_mensagens(gerenciador_de_conexao: stomp.Connection):
-    print("executando mostrar_quantidade_de_mensagens")
-
-
-def assinar_topico(gerenciador_de_conexao: stomp.Connection):
-    print("executando assinar_topico")
-
-
-def enviar_mensagem_para_cliente(gerenciador_de_conexao: stomp.Connection):
-    print("executando enviar_mensagem_para_cliente")
-
-
-def enviar_mensagem_para_topico(gerenciador_de_conexao: stomp.Connection):
-    print("executando enviar_mensagem_para_topico")
+    print("executando criar_topico")
 
 
 def modo_exemplo_gerente_da_fila():
-    print("executando gerente da fila")
     gerenciador_de_conexao: stomp.Connection = stomp.Connection()
 
     print("Iniciando conexão com ActiveMQ")
@@ -51,22 +28,28 @@ def modo_exemplo_gerente_da_fila():
     while executando:
         try:
             print("=====================================")
-            print("1 - Criar fila")
-            print("2 - Criar tópico")
-            print("3 - Mostrar quantidade de mensagens nas filas/tópicos")
+            print("1 - Listar filas e quantidades de mensagens")
+            print("2 - Listar topicos e quantidades de mensagens")
+            print("3 - Criar fila")
+            print("4 - Remover fila")
+            print("5 - Criar tópico")
+            print("6 - Remover tópico")
             print("0 - Sair")
             print("=====================================")
+
             opcao: str = input("Escolha uma opção: ")
+
             if opcao == "1":
-                criar_fila(gerenciador_de_conexao)
-            elif opcao == "2":
-                criar_topico(gerenciador_de_conexao)
+                listar_filas_e_qtd_mensagens(gerenciador_de_conexao)
             elif opcao == "3":
-                mostrar_quantidade_de_mensagens(gerenciador_de_conexao)
+                criar_fila(gerenciador_de_conexao)
+            elif opcao == "5":
+                criar_topico(gerenciador_de_conexao)
             elif opcao == "0":
                 executando = False
             else:
                 print("Opção inválida")
+
         except KeyboardInterrupt:
             executando = False
 
@@ -77,35 +60,3 @@ def modo_exemplo_gerente_da_fila():
 
 def modo_exemplo_cliente_da_fila():
     print("executando cliente da fila")
-    gerenciador_de_conexao: stomp.Connection = stomp.Connection()
-
-    print("Iniciando conexão com ActiveMQ")
-    gerenciador_de_conexao.connect(username="admin", passcode="admin", wait=True)
-    print("Conectado")
-
-    executando: bool = True
-    while executando:
-        try:
-            print("=====================================")
-            print("1 - Assinar tópico")
-            print("2 - Enviar mensagem para outro cliente")
-            print("3 - Enviar mensagem para tópico")
-            print("0 - Sair")
-            print("=====================================")
-            opcao: str = input("Escolha uma opção: ")
-            if opcao == "1":
-                assinar_topico(gerenciador_de_conexao)
-            elif opcao == "2":
-                enviar_mensagem_para_cliente(gerenciador_de_conexao)
-            elif opcao == "3":
-                enviar_mensagem_para_topico(gerenciador_de_conexao)
-            elif opcao == "0":
-                executando = False
-            else:
-                print("Opção inválida")
-        except KeyboardInterrupt:
-            executando = False
-
-    print("Encerrando conexão")
-    gerenciador_de_conexao.disconnect()
-    print("Encerrado")
