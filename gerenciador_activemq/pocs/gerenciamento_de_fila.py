e6ae7333f21f2233e3cc556786778229f17eeefe
Rafael Coelho <rafaellcoellho@gmail.com>
2023-04-17 22:23:28 -0300

    implementa listar filas, adicionar fila e remover fila

--- gerenciador_activemq/pocs/gerenciamento_de_fila.py
+++ gerenciador_activemq/pocs/gerenciamento_de_fila.py
@@ -1,5 +1,20 @@
+import json
+import re
+from typing import Dict, Tuple, Any, List
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+
 def modo_exemplo_gerente_da_fila():
     print("Iniciando gerente...")
+
+    url_base: str = "http://127.0.0.1:8161/api/jolokia"
+    objeto_do_broker: str = "org.apache.activemq:type=Broker,brokerName=localhost"
+
+    sessao: requests.Session = requests.Session()
+    sessao.headers.update({"Origin": "https://127.0.0.1"})
+    sessao.auth = HTTPBasicAuth(username="admin", password="admin")
 
     executando: bool = True
     while executando:
@@ -17,13 +32,53 @@ def modo_exemplo_gerente_da_fila():
             opcao: str = input("Escolha uma opção: ")
 
             if opcao == "1":
-                ...
+                resultado: requests.Response = sessao.post(
+                    url=url_base,
+                    json={
+                        "type": "read",
+                        "mbean": f"{objeto_do_broker},destinationType=Queue,destinationName=*",
+                        "attribute": ["Name", "QueueSize"],
+                    },
+                )
+                resposta: Dict[str, Any] = resultado.json()
+                filas: Dict[str, Dict[str, str]] = resposta["value"]
+                print(f"{'Nome da fila':<20} {'Mensagens':<10}")
+                for fila in filas.values():
+                    nome_da_fila: str = fila.get("Name")
+                    qtd_mensagens: str = fila.get("QueueSize")
+                    print(f"{nome_da_fila:<20} {qtd_mensagens:<10}")
             elif opcao == "2":
                 ...
             elif opcao == "3":
-                ...
+                nome_da_fila: str = input("Nome da fila: ")
+                resultado: requests.Response = sessao.post(
+                    url=url_base,
+                    json={
+                        "type": "exec",
+                        "mbean": objeto_do_broker,
+                        "operation": "addQueue",
+                        "arguments": [nome_da_fila],
+                    },
+                )
+                if resultado.status_code != 200:
+                    print("Erro ao criar fila!")
+                    print(resultado.json())
+                print("Fila criada com sucesso!")
             elif opcao == "4":
-                ...
+                nome_da_fila: str = input("Nome da fila: ")
+                resultado: requests.Response = sessao.post(
+                    url=url_base,
+                    json={
+                        "type": "exec",
+                        "mbean": objeto_do_broker,
+                        "operation": "removeQueue",
+                        "arguments": [nome_da_fila],
+                    },
+                )
+                if resultado.status_code != 200:
+                    print("Erro ao remover fila!")
+                    print(resultado.json())
+                print("Fila removida com sucesso!")
             elif opcao == "5":
                 ...
             elif opcao == "6":
